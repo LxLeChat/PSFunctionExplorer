4826d439b4741acfffbb2d12be50d0a69f730083
LxLeChat <yanaski@msn.com>
2019-05-01 12:39:38 +0200

    Expand seemed better thant export
    Thx @nicolasb ^^

--- PSFunctionExplorer/PSFunctionExplorer.psm1
+++ PSFunctionExplorer/PSFunctionExplorer.psm1
@@ -94,32 +94,32 @@ Class FUUtility {
 
 }
 
-Function Export-FUFile {
+Function Expand-FUFile {
     <#
     .SYNOPSIS
         Export a FUFunction to a ps1 file. It's like a reverse build process.
     .DESCRIPTION
         Export a FUFunction to a ps1 file. It's like a reverse build process.
     .EXAMPLE
-        PS C:\> Find-FUFunction -Path .\PSFunctionExplorer.psm1 | Export-FUFile
+        PS C:\> Find-FUFunction -Path .\PSFunctionExplorer.psm1 | Expand-FUFile
             Répertoire : C:\
 
 
         Mode                LastWriteTime         Length Name
         ----                -------------         ------ ----
-        -a----       30/04/2019     23:24            658 Export-Fufile.ps1
+        -a----       30/04/2019     23:24            658 Expand-FUFile.ps1
         -a----       30/04/2019     23:24           3322 Find-Fufunction.ps1
         -a----       30/04/2019     23:24           2925 Write-Fufunctiongraph.ps1
 
         Find all functions definitions inside PSFunctionExplorer.psm1 and save each function inside it's own ps1 file.
     .EXAMPLE
-        PS C:\> Find-FUFunction -Path .\PSFunctionExplorer.psm1 | Export-FUFile -Path C:\Temp
+        PS C:\> Find-FUFunction -Path .\PSFunctionExplorer.psm1 | Expand-FUFile -Path C:\Temp
             Répertoire : C:\Temp
 
 
         Mode                LastWriteTime         Length Name
         ----                -------------         ------ ----
-        -a----       30/04/2019     23:24            658 Export-Fufile.ps1
+        -a----       30/04/2019     23:24            658 Expand-FUFile.ps1
         -a----       30/04/2019     23:24           3322 Find-Fufunction.ps1
         -a----       30/04/2019     23:24           2925 Write-Fufunctiongraph.ps1
 
