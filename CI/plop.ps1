f85aeea860830edb7cd2569ca2bdd985ffb68b9d
LxLeChat <yanaski@msn.com>
2019-07-26 11:23:37 +0200

    Update plop.ps1

--- CI/plop.ps1
+++ CI/plop.ps1
@@ -3,6 +3,7 @@ $path= $(pwd).Path
 $PesterResults = invoke-pester $(join-Path -Path $path -ChildPath "PSFunctionExplorer\tests") -PassThru
 
 If ($PesterResults.PassedCount -eq $PesterResults.TotalCount ) {
+    write-host "LOLLILOL"
     ## Do Something
     $(test)
 }
